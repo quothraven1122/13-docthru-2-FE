ca76d9196a6a619e94e982022cee7fa0289f4441
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-07-13 16:53:37 +0900

    feat: #10 공통 컴포넌트 date input 생성

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 export default function Input({ suffix, errorMessage, className, ...inputProps }) {
   return (
-    <label>
+    <div>
       <div
         className={`px-[20px] py-[15px] rounded-[12px] flex border-1 border-gray-200 ${errorMessage && "border border-error"}`}
       >
@@ -8,6 +8,6 @@ export default function Input({ suffix, errorMessage, className, ...inputProps }
         {suffix}
       </div>
       {errorMessage && <p className="text-error text-[12px] mt-[8px]">{errorMessage}</p>}
-    </label>
+    </div>
   );
 }
