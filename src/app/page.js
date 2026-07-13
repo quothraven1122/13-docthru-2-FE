aeebc6cffe674d1dfbb50e77dc93c9b1f2f4cfe0
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-07-13 13:57:06 +0900

    feat: #10 공통 컴포넌트 input 생성

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,20 @@
 import Image from "next/image";
+import Input from "@/components/Input";
 
 export default function Home() {
-  return <div>페이지 입니다.</div>;
+  return (
+    <div>
+      페이지 입니다.
+      <div className="flex flex-col gap-[20px]">
+        <Input placeholder="이메일을 입력해주세요" />
+        <Input errorMessage="잘못된 이메일입니다." placeholder="이메일을 입력해주세요" />
+        <Input
+          type="password"
+          suffix={<Image width={24} height={24} src={"/icons/ic_btn_visibility_off.svg"} alt="아이콘" />}
+          placeholder="비밀번호를 입력해주세요"
+        />
+      </div>
+    </div>
+  );
+  x;
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+export default function Input({ suffix, errorMessage, className, ...inputProps }) {
+  return (
+    <label>
+      <div
+        className={`px-[20px] py-[15px] rounded-[12px] flex border-1 border-gray-200 ${errorMessage && "border border-error"}`}
+      >
+        <input {...inputProps} className={`${className} flex-1 outline-none text-gray-900 text-[16px] bg`}></input>
+        {suffix}
+      </div>
+      {errorMessage && <p className="text-error text-[12px] mt-[8px]">{errorMessage}</p>}
+    </label>
+  );
+}
