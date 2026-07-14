c4048c56861ad9a61b4295331942b114addde015
ydj-13 <de2013@naver.com>
2026-07-14 17:11:38 +0900

    chore: #19 마진부분 수정

--- src/components/ModalAlert.jsx
+++ src/components/ModalAlert.jsx
@@ -6,7 +6,7 @@ import ModalContainer from "./ModalContainer";
 export default function ModalAlert({ handleClose = () => {}, handleConfirm = () => {}, children }) {
   return (
     <ModalContainer handleClose={handleClose} type="alert">
-      <div className="font-pretendard text-[18px] font-[500] leading-normal text-gray-800 text-center mt-[108px]">
+      <div className="font-pretendard text-[18px] font-[500] leading-normal text-gray-800 text-center lg:mt-[108px] mt-[81px]">
         {children}
       </div>
       <div className="w-full flex lg:justify-end justify-center lg:mt-[45px] mt-[44px] mb-[28px] lg:mr-[28px]">
