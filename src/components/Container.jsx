c20fb09d249e2dcd4ef2ee816aeb584dddce9486
daeyeong1573 <rlaeodud9983@naver.com>
2026-07-14 10:36:08 +0900

    feat: #33 container 반응형 추가

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,13 +4,15 @@ import dateUtils from "@/utils/date";
 
 export default function Container({ date, maxMember, member, onViewOriginal, onChallenge }) {
   const challengeDisabled = dateUtils.isPastDeadline(date);
+  const buttonBaseClassName =
+    "flex flex-1 md:flex-none items-center justify-center whitespace-nowrap rounded-[12px] border-2 px-4 py-[0.44rem] text-[0.875rem] font-bold";
 
   return (
-    <div className="flex w-full max-w-md flex-col gap-4 rounded-2xl border-2 border-gray-100 bg-white p-5">
+    <div className="flex w-full flex-col gap-4 rounded-2xl border-2 border-gray-100 bg-white p-5 md:max-w-62.75 md:pt-7 md:pb-6 md:px-4 lg:max-w-71.25">
       <div className="flex items-center justify-center gap-4">
         <div className="flex items-center gap-1">
           <Image src="/icons/ic_deadline_s.svg" alt="마감일" width={16} height={16} />
-          <span className="text-[13px] font-normal text-gray-600">{date} 마감</span>
+          <span className="text-[13px] whitespace-nowrap font-normal text-gray-600">{date} 마감</span>
         </div>
         <div className="flex items-center gap-1">
           <Image src="/icons/ic_person_small.svg" alt="인원" width={16} height={16} />
@@ -20,11 +22,11 @@ export default function Container({ date, maxMember, member, onViewOriginal, onC
         </div>
       </div>
 
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 md:flex-col">
         <button
           type="button"
           onClick={onViewOriginal}
-          className="flex h-10 flex-1 items-center justify-center whitespace-nowrap rounded-full border-2 border-brand-dark bg-brand-yellow px-4 text-sm font-bold text-gray-900"
+          className={`${buttonBaseClassName} border-brand-dark bg-brand-yellow text-gray-900 md:px-[4.22rem]  lg:px-[5.28rem] `}
         >
           원문 보기
         </button>
@@ -32,7 +34,7 @@ export default function Container({ date, maxMember, member, onViewOriginal, onC
           type="button"
           onClick={onChallenge}
           disabled={challengeDisabled}
-          className="flex h-10 flex-1 items-center justify-center whitespace-nowrap rounded-full border-2 border-transparent bg-gray-900 px-4 text-sm font-bold text-white disabled:bg-gray-200 disabled:text-gray-400"
+          className={`${buttonBaseClassName} border-transparent bg-gray-900 text-white disabled:bg-gray-200 disabled:text-gray-400 md:px-[3.47rem] lg:px-[4.53rem]`}
         >
           작업 도전하기
         </button>
