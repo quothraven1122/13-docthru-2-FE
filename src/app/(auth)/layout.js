b45e963984ee4e6478b3a77c1c949abd615f0a9b
daeyeong1573 <rlaeodud9983@naver.com>
2026-07-15 09:47:24 +0900

    feat: #52 auth default파일 생성

---
+++ src/app/(auth)/layout.js
@@ -0,0 +1,22 @@
+import localFont from "next/font/local";
+import "../globals.css";
+
+const pretendard = localFont({
+  src: "../../../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  variable: "--font-pretendard-variable",
+  weight: "45 920",
+});
+
+export const metadata = {
+  title: "docthru",
+  description: "",
+};
+
+export default function AuthLayout({ children }) {
+  return (
+    <html lang="en" className={`${pretendard.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
