fb8135efce4e538cccb5eaf2afb859b20e917ea4
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-07-16 19:19:05 +0900

    feat: #12 add code highlight and allow tabbing to code tabbing

--- src/app/(main)/posts/_components/Editor.jsx
+++ src/app/(main)/posts/_components/Editor.jsx
@@ -4,11 +4,39 @@ import Underline from "@tiptap/extension-underline";
 import TextAlign from "@tiptap/extension-text-align";
 import Placeholder from "@tiptap/extension-placeholder";
 import Typography from "@tiptap/extension-typography";
-import { EditorContent, useEditor } from "@tiptap/react";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import { EditorContent, useEditor, ReactNodeViewRenderer } from "@tiptap/react";
 import { TextStyleKit } from "@tiptap/extension-text-style";
+import { createLowlight } from "lowlight";
 
 import { MenuBar } from "./EditorMenuBar.jsx";
-import { contentStyle, disabledMenuButtonStyle, activeMenuButtonStyle } from "./styles.js";
+import CodeBlockComponent from "./CodeBlockComponent.jsx";
+import { contentStyle, disabledMenuButtonStyle, activeMenuButtonStyle, codeBlockStyle } from "./styles.js";
+
+import xml from "highlight.js/lib/languages/xml";
+import css from "highlight.js/lib/languages/css";
+import scss from "highlight.js/lib/languages/scss";
+import javascript from "highlight.js/lib/languages/javascript";
+import typescript from "highlight.js/lib/languages/typescript";
+import json from "highlight.js/lib/languages/json";
+import bash from "highlight.js/lib/languages/bash";
+import http from "highlight.js/lib/languages/http";
+import sql from "highlight.js/lib/languages/sql";
+import graphql from "highlight.js/lib/languages/graphql";
+
+const lowlight = createLowlight();
+lowlight.register("html", xml);
+lowlight.register("css", css);
+lowlight.register("scss", scss);
+lowlight.register("javascript", javascript);
+lowlight.register("typescript", typescript);
+lowlight.register("jsx", javascript);
+lowlight.register("tsx", typescript);
+lowlight.register("json", json);
+lowlight.register("bash", bash);
+lowlight.register("http", http);
+lowlight.register("sql", sql);
+lowlight.register("graphql", graphql);
 
 const extensions = [
   TextStyleKit,
@@ -21,6 +49,20 @@ const extensions = [
   Placeholder.configure({
     placeholder: "번역 내용을 적어주세요",
   }),
+  CodeBlockLowlight.extend({
+    addNodeView() {
+      return ReactNodeViewRenderer(CodeBlockComponent);
+    },
+    addKeyboardShortcuts() {
+      return {
+        Tab: () => {
+          if (this.editor.isActive("codeBlock")) {
+            return this.editor.commands.insertContent("\t");
+          }
+        },
+      };
+    },
+  }).configure({ lowlight }),
 ];
 
 export default function Editor({ content, setContent }) {
@@ -41,7 +83,7 @@ export default function Editor({ content, setContent }) {
         disabledMenuButtonStyle={disabledMenuButtonStyle}
         activeMenuButtonStyle={activeMenuButtonStyle}
       />
-      <EditorContent editor={editor} className={contentStyle} />
+      <EditorContent editor={editor} className={`${contentStyle} ${codeBlockStyle}`} />
     </>
   );
 }
--- src/app/(main)/posts/_components/styles.js
+++ src/app/(main)/posts/_components/styles.js
@@ -7,14 +7,15 @@ export const contentStyle = `
   [&_.ProseMirror>p.is-editor-empty:first-child::before]:h-0
 
   [&>:first-child]:mt-0
-  [&_ul]:list-disc
-  [&_ol]:list-decimal
-  [&_ul]:pl-4
-  [&_ol]:pl-4
+
   [&_ul]:my-5
   [&_ol]:my-5
   [&_ul]:mr-4
   [&_ol]:mr-4
+  [&_ul]:list-disc
+  [&_ol]:list-decimal
+  [&_ul]:pl-4
+  [&_ol]:pl-4
   [&_li_p]:my-1
 
   [&_h1]:mt-14
@@ -42,25 +43,6 @@ export const contentStyle = `
   [&_h6]:mt-10
   [&_h6]:text-base
   [&_h6]:leading-tight
-
-  [&_code]:rounded-md
-  [&_code]:bg-purple-100
-  [&_code]:px-[0.3em]
-  [&_code]:py-[0.25em]
-  [&_code]:text-[0.85rem]
-  [&_code]:text-black
-
-  [&_pre]:my-6
-  [&_pre]:rounded-lg
-  [&_pre]:bg-black
-  [&_pre]:px-4
-  [&_pre]:py-3
-  [&_pre]:text-white
-
-  [&_pre_code]:bg-transparent
-  [&_pre_code]:p-0
-  [&_pre_code]:text-inherit
-  [&_pre_code]:text-[0.8rem]
 
   [&_blockquote]:my-6
   [&_blockquote]:border-l-[3px]
@@ -90,4 +72,59 @@ export const activeMenuButtonStyle = `
   justify-center
   items-center
   opacity-40
+`;
+
+export const codeBlockStyle = `
+  [&_.code-block]:relative
+
+  [&_.code-block_select]:absolute
+  [&_.code-block_select]:top-2
+  [&_.code-block_select]:right-2
+
+  [&_pre]:my-6
+  [&_pre]:rounded-lg
+  [&_pre]:bg-black
+  [&_pre]:px-4
+  [&_pre]:py-3
+  [&_pre]:font-mono
+  [&_pre]:text-white
+
+  [&_pre_code]:bg-transparent
+  [&_pre_code]:p-0
+  [&_pre_code]:text-inherit
+  [&_pre_code]:text-[0.8rem]
+
+  [&_.hljs-comment]:text-[#616161]
+  [&_.hljs-quote]:text-[#616161]
+
+  [&_.hljs-variable]:text-[#f98181]
+  [&_.hljs-template-variable]:text-[#f98181]
+  [&_.hljs-attribute]:text-[#f98181]
+  [&_.hljs-tag]:text-[#f98181]
+  [&_.hljs-name]:text-[#f98181]
+  [&_.hljs-regexp]:text-[#f98181]
+  [&_.hljs-link]:text-[#f98181]
+  [&_.hljs-selector-id]:text-[#f98181]
+  [&_.hljs-selector-class]:text-[#f98181]
+
+  [&_.hljs-number]:text-[#fbbc88]
+  [&_.hljs-meta]:text-[#fbbc88]
+  [&_.hljs-built_in]:text-[#fbbc88]
+  [&_.hljs-builtin-name]:text-[#fbbc88]
+  [&_.hljs-literal]:text-[#fbbc88]
+  [&_.hljs-type]:text-[#fbbc88]
+  [&_.hljs-params]:text-[#fbbc88]
+
+  [&_.hljs-string]:text-[#b9f18d]
+  [&_.hljs-symbol]:text-[#b9f18d]
+  [&_.hljs-bullet]:text-[#b9f18d]
+
+  [&_.hljs-title]:text-[#faf594]
+  [&_.hljs-section]:text-[#faf594]
+
+  [&_.hljs-keyword]:text-[#70cff8]
+  [&_.hljs-selector-tag]:text-[#70cff8]
+
+  [&_.hljs-emphasis]:italic
+  [&_.hljs-strong]:font-bold
 `;
