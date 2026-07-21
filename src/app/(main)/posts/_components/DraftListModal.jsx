1c94fbd792537e59e29fc4328228c019066af8c2
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-07-21 18:10:53 +0900

    feat: #12 임시저장 목록 배열화 및 nanoid 기반 ID 적용

--- src/app/(main)/posts/_components/DraftListModal.jsx
+++ src/app/(main)/posts/_components/DraftListModal.jsx
@@ -9,18 +9,28 @@ export default function DraftListModal({ draftList, onConfirm = () => {}, onDele
     <ModalContainer handleClose={onClose} className="w-[450px] gap-0">
       <header className="w-full flex justify-between">
         <h1 className="text-[16px] font-semibold">임시저장 글</h1>
-        <Image width={24} height={24} alt="취소 버튼 아이콘" src="/icons/ic_out_circle_m.svg" onClick={onClose} />
+
+        <Image
+          width={24}
+          height={24}
+          alt="취소 버튼 아이콘"
+          src="/icons/ic_out_circle_m.svg"
+          onClick={onClose}
+          className="cursor-pointer"
+        />
       </header>
-      <p className="w-full text-[12px] font-normal mt-[28px] mb-[16px]">총 {Object.keys(draftList).length}개</p>
+
+      <p className="w-full text-[12px] font-normal mt-[28px] mb-[16px]">총 {draftList.length}개</p>
+
       <div className="w-full h-[280px] overflow-auto divide-y divide-gray-200">
-        {Object.entries(draftList).map(([key, value]) => {
+        {draftList.map((draft) => {
           return (
             <div
-              key={key}
+              key={draft.id}
               onClick={() => {
-                onConfirm(value.content);
+                onConfirm(draft.content);
               }}
-              className="h-[80px] flex items-start px-[10px] py-[12px] cursor-pointer"
+              className="h-fit flex items-start px-[10px] py-[12px] cursor-pointer"
             >
               <Image
                 width={20}
@@ -29,17 +39,21 @@ export default function DraftListModal({ draftList, onConfirm = () => {}, onDele
                 src="/icons/ic_out_circle_m.svg"
                 onClick={(e) => {
                   e.stopPropagation();
-                  onDelete(key);
+
+                  onDelete(draft.id);
                   onClose();
                 }}
                 className="mr-[10px]"
               />
+
               <div>
-                <h2 className="text-[14px] text-gray-800 font-medium">{textUtils.truncateText(value.title, 35)}</h2>
+                <h2 className="text-[14px] text-gray-800 font-medium">{textUtils.truncateText(draft.title, 35)}</h2>
+
                 <p className="text-[12px] text-gray-400 font-normal">
-                  {textUtils.truncateText(value.content.text, 30)}
+                  {textUtils.truncateText(draft.content.text, 30)}
                 </p>
-                <p className="text-[12px] text-gray-400 font-normal">{dateUtils.format(value.createdAt, "dot")}</p>
+
+                <p className="text-[12px] text-gray-400 font-normal">{dateUtils.format(draft.createdAt, "dot")}</p>
               </div>
             </div>
           );
--- src/app/(main)/posts/page.js
+++ src/app/(main)/posts/page.js
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { nanoid } from "nanoid";
 
 import { useModal } from "@/providers/ModalProvider";
 import Spinner from "@/components/Spinner";
@@ -17,46 +18,53 @@ const src = "https://en.wikipedia.org/wiki/D.Va";
 
 export default function TranslationWritePage() {
   const { openModal, closeModal } = useModal();
+
   const [isToastOpen, setIsToastOpen] = useState(false);
   const [isIframeOpen, setIsIframeOpen] = useState(false);
   const [isIframeLoading, setIsIframeLoading] = useState(true);
   const [content, setContent] = useState({ json: null, text: "" });
-  const [draftList, setDraftList] = useState({});
+  const [draftList, setDraftList] = useState([]);
   const [reveal, setReveal] = useState(false);
 
+  //임시 저장 관련 함수들
   const getDrafts = () => {
     const allRelatedDrafts = Object.entries({ ...localStorage })
-      .filter(([key, value]) => JSON.parse(value).title === title)
-      .reduce((prev, [key, value]) => {
-        prev[key] = JSON.parse(value);
-        return prev;
-      }, {});
+      .map(([id, value]) => ({
+        id,
+        ...JSON.parse(value),
+      }))
+      .filter((draft) => draft && draft.title === title)
+      .sort((draft1, draft2) => new Date(draft2.createdAt) - new Date(draft1.createdAt));
+
     setDraftList(allRelatedDrafts);
+
     return allRelatedDrafts;
   };
   const saveDraft = () => {
-    const allDrafts = { ...localStorage };
-    const currentIndex = Object.keys(allDrafts).length;
-    const draft = JSON.stringify({ title, content, createdAt: new Date() });
-    localStorage.setItem(currentIndex + 1, draft);
-    setDraftList((prev) => ({ ...prev, [currentIndex + 1]: JSON.parse(draft) }));
+    const id = nanoid();
+    const draft = {
+      id,
+      title,
+      content,
+      createdAt: new Date().toISOString(),
+    };
+
+    localStorage.setItem(id, JSON.stringify(draft));
+    setDraftList((prev) => [draft, ...prev]);
+
     setReveal(true);
   };
-  const deleteDraft = (key) => {
-    localStorage.removeItem(key);
+  const deleteDraft = (id) => {
+    localStorage.removeItem(id);
 
-    setDraftList((prev) => {
-      const next = { ...prev };
-      delete next[key];
-      return next;
-    });
+    setDraftList((prev) => prev.filter((draft) => draft.id !== id));
   };
 
+  // 최초 렌더링 시 임시저장 목록 확인
   useEffect(() => {
     // eslint-disable-next-line react-hooks/set-state-in-effect
     const allDrafts = getDrafts();
-
-    if (Object.keys(allDrafts).length === 0) return;
+    if (allDrafts.length === 0) return;
     setIsToastOpen(true);
   }, []);
 
@@ -68,8 +76,9 @@ export default function TranslationWritePage() {
       )}
     >
       <div className="flex-1 overflow-y-auto md:overflow-y-visible">
-        <header className="flex flex-wrap gap-y-[10px] justify-between items-center m-auto py-[24px] ">
+        <header className="flex flex-wrap gap-y-[10px] justify-between items-center m-auto py-[24px]">
           <Image width={120} height={30} src="/logos/logo.svg" alt="로고" className="cursor-pointer" />
+
           <div className="flex gap-[8px]">
             <button
               className={cn("bg-[#ffe7e7] text-[#f24744] flex cursor-pointer whitespace-nowrap", buttonCommonStyle)}
@@ -77,12 +86,14 @@ export default function TranslationWritePage() {
               <p className="hidden md:block">포기</p>
               <Image width={20} height={20} src="/icons/ic_exit.svg" alt="나가기 아이콘" />
             </button>
+
             <button
               onClick={saveDraft}
               className={cn("border border-gray-800 cursor-pointer whitespace-nowrap", buttonCommonStyle)}
             >
               임시저장
             </button>
+
             <button className={`text-white bg-brand-dark cursor-pointer whitespace-nowrap ${buttonCommonStyle}`}>
               제출하기
             </button>
@@ -90,6 +101,7 @@ export default function TranslationWritePage() {
         </header>
 
         <h1 className="text-[20px] text-gray-800 font-semibold pb-[24px] border-b border-b-gray-200">{title}</h1>
+
         <div className="overflow-hidden">
           <div
             className={cn(
@@ -104,6 +116,7 @@ export default function TranslationWritePage() {
 
         <div className="relative">
           <Editor content={content.json} setContent={setContent} />
+
           {!isIframeOpen && (
             <button
               onClick={() => {
@@ -112,18 +125,20 @@ export default function TranslationWritePage() {
               }}
               className="flex gap-[4px] fixed right-0 top-[25%] px-[10px] py-[14px] rounded-l-[24px] border-t border-b border-l border-gray-100 z-fixed text-[14px] text-gray-500 font-semibold cursor-pointer shadow-[0_4px_4px_0_rgba(88,92,130,0.05)] bg-white"
             >
-              <Image width={24} height={24} alt="원문 보기 버튼" src={"/icons/ic_list.svg"} />
+              <Image width={24} height={24} alt="원문 보기 버튼" src="/icons/ic_list.svg" />
               <p>원문</p>
             </button>
           )}
         </div>
       </div>
+
       {isIframeOpen && (
         <div className="flex flex-1 min-w-0 min-h-0 flex-col">
           <header className="w-full h-[48px] bg-gray-400 flex justify-between items-center px-[16px] py-[8px]">
             <button onClick={() => setIsIframeOpen(false)} className="cursor-pointer">
-              <Image width={32} height={32} alt="iframe 나가기 버튼 아이콘" src={"/icons/ic_out_circle_m.svg"} />
+              <Image width={32} height={32} alt="iframe 나가기 버튼 아이콘" src="/icons/ic_out_circle_m.svg" />
             </button>
+
             <Link
               href={src}
               target="_blank"
@@ -133,16 +148,19 @@ export default function TranslationWritePage() {
               <Image width={24} height={24} alt="링크 열기 아이콘" src="/icons/ic_click.svg" />
             </Link>
           </header>
+
           <div className="relative w-full h-full flex justify-center items-center">
             {isIframeLoading && (
               <div className="absolute inset-0 z-10 flex items-center justify-center bg-white/80">
                 <Spinner />
               </div>
             )}
-            <iframe src={src} onLoad={() => setIsIframeLoading(false)} className="w-full h-full flex-1"></iframe>
+
+            <iframe src={src} onLoad={() => setIsIframeLoading(false)} className="w-full h-full flex-1" />
           </div>
         </div>
       )}
+
       {isToastOpen && (
         <DraftToast
           content="임시 저장된 작업물이 있어요. 저장된 작업물을 불러오시겠어요?"
@@ -156,7 +174,7 @@ export default function TranslationWritePage() {
                   closeModal();
                   setIsToastOpen(false);
                 }}
-                onDelete={(key) => deleteDraft(key)}
+                onDelete={deleteDraft}
                 onClose={closeModal}
               />,
             )
