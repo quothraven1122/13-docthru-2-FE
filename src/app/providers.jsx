d927ea732b35d8223a280d2cc12bb3db4379cfd6
bangminjae <abab1070@naver.com>
2026-07-24 11:15:57 +0900

    feat: #89 리액트쿼리 프로바이더 설정

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,6 +1,16 @@
+"use client";
+
 import ModalProvider from "@/providers/ModalProvider";
-import React from "react";
+import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function Providers({ children }) {
-  return <ModalProvider>{children}</ModalProvider>;
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <ModalProvider>{children}</ModalProvider>;
+    </QueryClientProvider>
+  );
 }
